087f9a7ab5630ee03814fc78ad6343b006e40d24
alessia-carfi <alessia.carfi@studio.unibo.it>
2022-12-09 15:41:31 +0100

    finished exercise 2

--- 2-ComplexAlgebra/Complex.cs
+++ 2-ComplexAlgebra/Complex.cs
@@ -1,5 +1,6 @@
 namespace ComplexAlgebra
 {
+    using System;
     /// <summary>
     /// A type for representing Complex numbers.
     /// </summary>
@@ -17,6 +18,63 @@ namespace ComplexAlgebra
     /// TODO:     - e.g. via the Equals(object) method
     public class Complex
     {
-        // TODO: fill this class\
+        public double Real { get; set; }
+        public double Imaginary { get; set; }
+
+        public double Modulus => Math.Sqrt(Math.Pow(Real, 2) + Math.Pow(Imaginary, 2));
+        public double Phase => Math.Atan2(Imaginary, Real);
+    
+        public Complex(double re, double im)
+        {
+            Real = re;
+            Imaginary = im;
+        }
+
+        public Complex Complement()
+        {
+            return new Complex(Real, -Imaginary);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Complex complex &&
+                   Real == complex.Real &&
+                   Imaginary == complex.Imaginary &&
+                   Modulus == complex.Modulus &&
+                   Phase == complex.Phase;
+        }
+
+        public override string ToString()
+        {   
+            if (Real == 0 && Imaginary == 0)
+                return "0.0";
+            String result = Real == 0 ? "" : Real.ToString();
+            if (Imaginary == 0)
+                return result;
+            result += Imaginary > 0 ? $"{(Real > 0 ? "+" : "")}{(Imaginary > 1 ? Imaginary : "")}i" : $"-{(Imaginary > 1 ? Imaginary : "")}i";
+            return result;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public static Complex operator +(Complex num1, Complex num2)
+        {
+            Complex result = new Complex(0, 0);
+            result.Real = num1.Real + num2.Real;
+            result.Imaginary = num1.Imaginary + num2.Imaginary;
+            return result;
+        }
+
+        public static Complex operator -(Complex num1, Complex num2)
+        {
+            Complex result = new Complex(0, 0);
+            result.Real = num1.Real - num2.Real;
+            result.Imaginary = num1.Imaginary - num2.Imaginary;
+            return result;
+        }
+
     }
 }
